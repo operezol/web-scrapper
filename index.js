b080f3fb7130fe252fccebc961c91dffac0329cc
operezol <oriolperezolivares@gmail.com>
2021-10-17 12:56:38 +0200

    iterates through found pages

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 const puppeteer = require("puppeteer")
 const domain = "https://www.fxstreet.com"
-let dataList = [{ url: "https://www.fxstreet.com", visited: false, classNames: [] }]
+let dataList = [{ url: "https://www.fxstreet.com", visited: false, classNameFound: true }]
 
-async function processPage() {
+async function processPage(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    const url = dataList.find(page => page.visited === false);
-    if (url) {
-        await page.goto(url.url)
+    await page.goto(url)
 
-        let urls = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll("a")).map(x => x.href)
-        })
+    let urls = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("a")).map(x => x.href)
+    })
 
-        await browser.close()
+    await browser.close()
 
-        urls = urls.filter(url => url.startsWith(domain) || url.startsWith("/")).map(url => {
-            url = url.includes('#')?url.substring(0, url.indexOf('#')):url
-            url = url.includes('?')?url.substring(0, url.indexOf('?')):url
-            if (url.startsWith(domain)) return url
-            else if (url.startsWith("/")) return domain + url
-        })
+    urls = urls.filter(url => url.startsWith(domain) || url.startsWith("/")).map(url => {
+        url = url.includes('#') ? url.substring(0, url.indexOf('#')) : url
+        url = url.includes('?') ? url.substring(0, url.indexOf('?')) : url
+        if (url.startsWith(domain)) return url
+        else if (url.startsWith("/")) return domain + url
+    })
 
-        // check if urls are already in datalist. 
-        // Only include if not
-        // Set current processed url object key visited to true 
-        // Set current processed url object classFound visited to true if class is found
-        // Check if are still urls not visited
-        // if true recursive call
+    urls.forEach(url => {
+        const found = dataList.some(item => item.url === url)
+        if (!found) dataList.push({ url: url, visited: false, classNameFound: true })
+    })
+    dataList[dataList.findIndex(item => item.url === url)] = { url: url, visited: true, classNameFound: true };
 
-
-        dataList = urls
-    } else { return false }
+    // Set current processed url object classFound visited to true if class is found
+    const newUrl = dataList.find(page => page.visited === false);
+    console.log('new url: ',newUrl.url)
+    if (newUrl) {
+        await processPage(newUrl.url)
+    }
 }
 (async () => {
     try {
-        await processPage().catch(err => console.log(err))
+        const entryUrl = dataList[0].url
+        console.log('entryUrl: ',entryUrl)
+        await processPage(entryUrl).catch(err => console.log(err))
         console.log(dataList)
     } catch (e) {
         console.log(e)
